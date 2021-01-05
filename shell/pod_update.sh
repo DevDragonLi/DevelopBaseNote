3a67ba915acc113ae8e278a2f3ea02f802c250a9
dragonli <dragonli_52171@163.com>
2021-01-05 11:35:48 +0800

    子组件更新repo自定义

--- shell/pod_update.sh
+++ shell/pod_update.sh
@@ -1,3 +1,4 @@
 # private source cmd 
 source = 'ssh:xxx'
-pod repo push ZDPodSpecs --sources=${source} --swift-version=5.0 --skip-import-validation --skip-tests --use-json --allow-warnings --verbose
+podRepo = 'podRepo'
+pod repo push ${podRepo} --sources=${source} --swift-version=5.0 --skip-import-validation --skip-tests --use-json --allow-warnings --verbose
